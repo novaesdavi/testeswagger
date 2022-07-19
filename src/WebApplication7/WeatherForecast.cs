9bd54d3f1ce8c9ff137d00762a07c97c7308c835
Davi Oliveira <novaes.davi1@gmail.com>
2022-07-18 23:39:19 -0300

    .

--- src/WebApplication7/WeatherForecast.cs
+++ src/WebApplication7/WeatherForecast.cs
@@ -2,10 +2,13 @@ using System;
 
 namespace WebApplication7
 {
+    /// <summary>objeto de temperatura</summary>
     public class WeatherForecast
     {
+        /// <summary>Data de quando a temperatura se refere</summary>
         public DateTime Date { get; set; }
 
+        /// <summary>temperatura em Celsius</summary>
         public int TemperatureC { get; set; }
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
